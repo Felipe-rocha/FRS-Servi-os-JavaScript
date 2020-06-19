ac460c7574d2d82ed2d996662c9380a90cc20f04
Felipe Rocha <rochadsf@gmail.com>
2020-06-19 01:08:36 -0300

    Alteração de Quadro com click e Habilitado o botão Voltar ao topo

--- inicial.js
+++ inicial.js
@@ -1,1 +1,54 @@
-var itensMenu = document.querySelectorAll(".itens-header");
+var voltaPaginaTopo = document.querySelector(".volta-topo");
+
+var pegaProjetosGuia = document.querySelector(".quadro-projetos");
+
+var linkGuia = document.querySelector(".quadro-guiaotimiza");
+var pegaOnsale = document.querySelector(".quadro-onsale");
+var pegaEdicao = document.querySelector(".quadro-edicao");
+
+
+voltaPaginaTopo.addEventListener("click", function(){
+    window.scrollTo(0, 0);
+});
+
+pegaProjetosGuia.addEventListener("click", function(){
+    incluiTextosOcultos();
+    var excluiQuadros = document.querySelector(".quadros-inicial");
+    excluiQuadros.classList.add("remove-tela");
+})
+
+linkGuia.addEventListener("click", function(){
+    window.location.assign("https://frsservicos.herokuapp.com/Projeto_Guia_Otimiza/guia.html");
+})
+
+pegaOnsale.addEventListener("click", function(){
+    window.location.assign("https://frsservicos.herokuapp.com/Projeto_Guia_Otimiza/guia.html");
+})
+
+pegaEdicao.addEventListener("click", function(){
+    window.location.assign("https://frsservicos.herokuapp.com/Projeto_Guia_Otimiza/guia.html");
+})
+
+function incluiTextosOcultos (){
+    var pegaQuadroOculto = document.querySelectorAll(".remove-tela");
+
+    for ( var i = 0 ; i < pegaQuadroOculto.length; i++){
+        pegaQuadroOculto[i].classList.remove("remove-tela");
+        console.log(i);
+    };
+}
+
+
+// pegaQuadroOculto.classList.add(".quadros-inicial");
+
+// for (var itensQuadros; i = 0; i++){
+    //     incluiQuadro.classList.remove("remove-tela");
+    // }
+
+// pegaQuadroOculto.forEach(function () {
+//     pegaQuadroOculto[0].remove(".remove-tela");
+// });
+
+
+
+
